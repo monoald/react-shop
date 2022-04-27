eb5badb5b82172c5d737e5ee027bd72349b1c477
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2022-04-27 11:27:38 -0400

    Web structured

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,12 @@ import Layout from '../containers/Layout';
 import Login from '../pages/Login';
 import PasswordRecovery from '../pages/PasswordRecovery';
 import '../styles/global.css';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 
 const App = () => {
   return (
@@ -14,6 +20,12 @@ const App = () => {
           <Route exact path='/' element={<Home/>} />
           <Route exact path='/login' element={<Login/>} />
           <Route exact path='/recovery-password' element={<PasswordRecovery/>} />
+          <Route exact path='/send-email' element={<SendEmail/>} />
+          <Route exact path='/new-password' element={<NewPassword/>} />
+          <Route exact path='/account' element={<MyAccount/>} />
+          <Route exact path='/signup' element={<CreateAccount/>} />
+          <Route exact path='/checkout' element={<Checkout/>} />
+          <Route exact path='/orders' element={<Orders/>} /> 
           <Route path='*' element={<NotFound/>} />
         </Routes>
       </Layout>
